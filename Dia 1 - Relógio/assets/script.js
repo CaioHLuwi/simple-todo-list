0c4f36044bc4bcf278d2e1284d363b1bafa6b8b2
CaioHLuwi <caiohenriquelw2004@gmail.com>
2024-12-07 23:04:39 -0300

    Add digital clock funcionality

--- Dia 1 - Relógio/assets/script.js
+++ Dia 1 - Relógio/assets/script.js
@@ -1,10 +1,19 @@
-function adicionaNomes(nomes, ...nomesNovos){
-    let nomesConjuntos = [
-        ...nomes,
-        ...nomesNovos
-    ]
-    return console.log(nomesConjuntos);
+let digitalElement = document.querySelector('.digital');
+let secondsElement = document.querySelector('.p_s');
+let minutesElement = document.querySelector('p_m');
+let hoursElement = document.querySelector('p_h');
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.textContent = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 }
 
-let nomes = ['Caio', 'Pedro'];
-adicionaNomes(nomes, 'Katarina', 'Luiz');
+function fixZero(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateClock, 1000);
