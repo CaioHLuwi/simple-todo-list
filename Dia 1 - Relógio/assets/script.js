0c8aecd98da94fea776f127087034fe08551ba9b
CaioHLuwi <caiohenriquelw2004@gmail.com>
2024-12-07 23:22:51 -0300

    Add feature to analogic clock funcionality

--- Dia 1 - Relógio/assets/script.js
+++ Dia 1 - Relógio/assets/script.js
@@ -1,7 +1,7 @@
 let digitalElement = document.querySelector('.digital');
 let secondsElement = document.querySelector('.p_s');
-let minutesElement = document.querySelector('p_m');
-let hoursElement = document.querySelector('p_h');
+let minutesElement = document.querySelector('.p_m');
+let hoursElement = document.querySelector('.p_h');
 
 function updateClock() {
     let now = new Date();
@@ -10,6 +10,14 @@ function updateClock() {
     let second = now.getSeconds();
 
     digitalElement.textContent = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+    let secondDeg = ((360 / 60) * second) - 90;
+    let minuteDeg = ((360 / 60) * minute) - 90;
+    let hourDeg = ((360 / 12) * hour) - 90;
+
+    secondsElement.style.transform = `rotate(${secondDeg}deg)`;
+    minutesElement.style.transform = `rotate(${minuteDeg}deg)`;
+    hoursElement.style.transform = `rotate(${hourDeg}deg)`;
 }
 
 function fixZero(time){
@@ -17,3 +25,4 @@ function fixZero(time){
 }
 
 setInterval(updateClock, 1000);
+updateClock();
